f958774d8a91b850cd2725ebf7c9534e303527b4
angkisuk-svg <angkisuk@gmail.com>
2026-07-06 20:16:40 +0900

    링크 복사 기능 새로운 완벽 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -70,8 +70,8 @@ function ProductContent() {
       </header>
 
       <section className="bg-slate-800 p-5 mb-4 shadow-xl border-b border-slate-700">
-        <div className="bg-orange-500/20 text-orange-400 text-xs font-bold px-4 py-1 inline-block rounded mb-3 border border-orange-500/30">
-          이포스팅은 쿠팡파트너스의 활동 일환으로 수수료를 받습니다.
+        <div className="bg-orange-500/20 text-white-400 text-base font-bold px-2 py-1 inline-block rounded mb-3 border border-orange-500/30">
+          이포스팅은 쿠팡파트너스의 활동으로 수수료를 받습니다
         </div>
         
         <a 
